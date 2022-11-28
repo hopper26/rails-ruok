78874c0bb523dfde2ef1bdc6512c92e43a84ce1e
Hayley Lucas <halucas93@gmail.com>
2022-11-28 21:54:15 +1100

    hmm

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -15,8 +15,13 @@ class EmotionsController < ApplicationController
   def counter
     @emotion_h = current_user.emotions.where(feeling: "happy").count
     @emotion_m = current_user.emotions.where(feeling: "meh").count
-    @emotion_s = current_user.emotions.where(feeling: "sad").count
+    @emotion_s = Emotion.where(feeling: "sad", user_id: current_user.id).count
+    # @emotion_s = current_user.emotions.where(feeling: "sad").count
     # Emotion.where(feeling: "sad", user_id: current_user.id).count
+
+    # ERROR ON HEROKU BUT WORKS LOCALLY
+    # 2022-11-28T10:51:38.355079+00:00 app[web.1]: [92608fd0-cd31-48b4-bf37-dddb130a0634] ActiveRecord::StatementInvalid (PG::UndefinedColumn: ERROR:  column emotions.feeling does not exist
+    # 2022-11-28T10:51:38.355080+00:00 app[web.1]: LINE 1: ...OM "emotions" WHERE "emotions"."user_id" = $1 AND "emotions"...
   end
 
   private
