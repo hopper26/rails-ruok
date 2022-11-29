51bda8fd4eda03dd4ae642afec66782ad65fed1d
Hayley Lucas <halucas93@gmail.com>
2022-11-30 08:42:55 +1100

    progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,41 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path, status: :see_other
+    else
+      render :new, status: :unprocessable_entity, alert: "issue"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Entry deleted"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
