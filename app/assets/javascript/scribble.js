baacd72092667bbcc1a587484a223dafe04296cf
Hayley Lucas <halucas93@gmail.com>
2022-12-04 08:37:23 +1100

    fix not being able to draw

--- app/assets/javascript/scribble.js
+++ app/assets/javascript/scribble.js
@@ -1,4 +1,5 @@
-
+//document.getElementById('tab_2').onclick = function ()
+function lets_draw() {
   let points = new Array();
   colorInput = document.getElementById("color");
   lineInput = document.getElementById("line-width");
@@ -11,38 +12,36 @@
   canvas.addEventListener("mouseup", finishedPosition);
   canvas.addEventListener("mousemove", draw);
 
+  function startPosition(event) {
+    const mouseX = event.clientX - canvas.getBoundingClientRect().left;
+    const mouseY = event.clientY - canvas.getBoundingClientRect().top;
+    painting = true;
+    ctx.lineWidth = lineInput.value;
+    ctx.strokeStyle = colorInput.value;
+    ctx.beginPath();
+    ctx.moveTo(mouseX, mouseY);
+    points[points.length] = [mouseX, mouseY];
+    // draw(event);
+  }
 
+  function finishedPosition(event) {
+    painting = false;
+    const s = JSON.stringify(points);
+    localStorage['lines'] = s;
+  }
 
+  function draw(event) {
+    if (!painting) return;
+    const mouseX = event.clientX - canvas.getBoundingClientRect().left;
+    const mouseY = event.clientY - canvas.getBoundingClientRect().top;
+    ctx.lineTo(mouseX, mouseY);
+    ctx.stroke();
+    ctx.lineWidth = lineInput.value;
+    ctx.strokeStyle = colorInput.value;
+    ctx.lineJoin = ctx.lineCap = 'round'
+    points[points.length] = [mouseX, mouseY];
+  }
 
-function startPosition(event) {
-  const mouseX = event.clientX - canvas.getBoundingClientRect().left;
-  const mouseY = event.clientY - canvas.getBoundingClientRect().top;
-  painting = true;
-  ctx.lineWidth = lineInput.value;
-  ctx.strokeStyle = colorInput.value;
-  ctx.beginPath();
-  ctx.moveTo(mouseX, mouseY);
-  points[points.length] = [mouseX, mouseY];
-  // draw(event);
-}
-
-function finishedPosition(event) {
-  painting = false;
-  const s = JSON.stringify(points);
-  localStorage['lines'] = s;
-}
-
-function draw(event) {
-  if (!painting) return;
-  const mouseX = event.clientX - canvas.getBoundingClientRect().left;
-  const mouseY = event.clientY - canvas.getBoundingClientRect().top;
-  ctx.lineTo(mouseX, mouseY);
-  ctx.stroke();
-  ctx.lineWidth = lineInput.value;
-  ctx.strokeStyle = colorInput.value;
-  ctx.lineJoin = ctx.lineCap = 'round'
-  points[points.length] = [mouseX, mouseY];
-}
 
 // saves image
 document.getElementById('save').onclick = function () {
@@ -66,4 +65,5 @@ document.getElementById('load').onclick = function () {
   myImage = new Image();
   myImage.src = imageURL;
   ctx.drawImage(myImage, 0, 0);
+}
 }
