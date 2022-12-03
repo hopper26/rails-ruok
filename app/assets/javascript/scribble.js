16eeb055e51c5a92c510bcc7997b657d5b29ea48
Hayley Lucas <halucas93@gmail.com>
2022-12-03 23:06:43 +1100

    save load

--- app/assets/javascript/scribble.js
+++ app/assets/javascript/scribble.js
@@ -12,6 +12,7 @@
 
 
 
+
 function startPosition(event) {
   const mouseX = event.clientX - canvas.getBoundingClientRect().left;
   const mouseY = event.clientY - canvas.getBoundingClientRect().top;
@@ -38,4 +39,27 @@ function draw(event) {
   ctx.strokeStyle = colorInput.value;
   ctx.lineJoin = ctx.lineCap = 'round'
   points[points.length] = [mouseX, mouseY];
+}
+
+// saves image
+document.getElementById('save').onclick = function () {
+  imageURL = canvas.toDataURL('image/png');
+}
+
+//clear canvas
+document.getElementById('clear').onclick = function () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+//loads it back
+document.getElementById('load').onclick = function () {
+  // var win = window.open();
+  // if(navigator.userAgent.indexOf("Chrome") == -1 ) {
+  //   win.location = imageURL;
+  // } else{
+  //   canvas.write('<iframe src="' + imageURL + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>')
+  // }
+  myImage = new Image();
+  myImage.src = imageURL;
+  ctx.drawImage(myImage, 0, 0);
 }
