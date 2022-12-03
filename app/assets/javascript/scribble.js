3d745474c6b5b9634ff4d219a1028ad1b05aa6fa
Hayley Lucas <halucas93@gmail.com>
2022-12-03 23:40:27 +1100

    load and clearing at the same time

--- app/assets/javascript/scribble.js
+++ app/assets/javascript/scribble.js
@@ -62,6 +62,7 @@ document.getElementById('load').onclick = function () {
   // } else{
   //   canvas.write('<iframe src="' + imageURL + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>')
   // }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   myImage = new Image();
   myImage.src = imageURL;
   ctx.drawImage(myImage, 0, 0);
