820555ad129f36df14b11cf02faee5c70de45a8b
Hayley Lucas <halucas93@gmail.com>
2022-12-28 05:27:22 +0000

    url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://www.ruok.quest" }
+  config.action_mailer.default_url_options = { host: "https://www.ruok.quest" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
